4f2b6e5f4e5533f1c8b771ee20c6ae7ec551daeb
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-05-01 14:48:59 -0300

    beryllium: powerstats: Pixel -> Xiaomi
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I92714a500abb48081327cde8999f6b6e1d831f1d

--- device.mk
+++ device.mk
@@ -328,7 +328,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel \
+    android.hardware.power.stats@1.0-service.xiaomi \
     android.hardware.power@1.2.vendor \
     libqti-perfd-client
 
