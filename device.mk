ce2c57c2e1ac9db9df7a1ad571619786560a960b
Albert I <kras@raphielgang.org>
2023-12-26 21:46:55 +0530

    beryllium: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- device.mk
+++ device.mk
@@ -370,6 +370,8 @@ PRODUCT_PACKAGES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
