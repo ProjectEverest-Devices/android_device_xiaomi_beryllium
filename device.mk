df371cde9e55fd16369a9672df95d402f7dee577
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    beryllium: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- device.mk
+++ device.mk
@@ -410,7 +410,7 @@ PRODUCT_USE_VNDK_OVERRIDE := true
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vibrator
 PRODUCT_PACKAGES += \
